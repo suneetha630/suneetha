24cbf2e9a0ccdd5da71d9848adab548ea0e995d6
suneetha630 <nagalasuneetha630@gmail.com>
2022-01-04 15:18:31 +0530

    commit

---
+++ Demo.java
@@ -0,0 +1,7 @@
+class Demo
+{
+public  static void main(String a[])
+{
+System.out.println("hi");
+}
+}
---
+++ tharun.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class hi
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("hello");
+}
+}
